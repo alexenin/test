de60986366aa11b9b0d36419c585e04bacdb6ea7
alexenin <alexenin@yandex.ru>
2017-12-24 22:06:44 +0300

    first commit

---
+++ Assets/Scripts/Clouds.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clouds : MonoBehaviour {
+
+    private float speed = 0.1f, yPos;
+
+    void Start()
+    {
+        speed = Random.Range(0, 10) > 5 ? speed : -speed;
+        yPos = transform.position.y; //Начальное значение по оси y
+
+    }
+
+    void Update()
+    {
+        if (transform.position.y >= yPos + 0.1f || transform.position.y <= yPos - 0.1f)
+            speed = -speed;
+        transform.position += new Vector3(0, speed * Time.deltaTime, 0); //Двигаем тучки
+    }
+}
---
+++ Assets/Scripts/Ship.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour {
+
+
+	// Update is called once per frame
+	void Update () {
+        float move =Input.GetAxis("Vertical"); //Кнопки w и s
+        float rotate = Input.GetAxis("Horizontal"); //Кнопки a и d
+        transform.Translate(Vector3.right * move * 2f * Time.deltaTime);
+        //transform.position = new Vector3 (transform.position.x, transform.position.y, transform.position.z - move * 2f * Time.deltaTime);
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y + rotate * 20f * Time.deltaTime, transform.eulerAngles.z);
+	}
+}
